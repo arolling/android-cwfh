d4ed039495886986d2ee3659267dbe82fb1310ed
Abigail Rolling and Brad London <arolling@gmail.com>
2016-04-21 10:27:56 -0700

    Show name of specifc tapped item in drawer

--- app/src/main/java/com/epicodus/localbusiness/MainActivity.java
+++ app/src/main/java/com/epicodus/localbusiness/MainActivity.java
@@ -2,8 +2,12 @@ package com.epicodus.localbusiness;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -26,5 +30,15 @@ public class MainActivity extends AppCompatActivity {
         String[] navArray = { "Home", "What We Do", "Get Involved", "How to Give", "Multi-Media", "About Us", "Blog", "Personal Blog"};
         mAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, navArray);
         mNavList.setAdapter(mAdapter);
+
+        mNavList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String page = ((TextView)view).getText().toString();
+                Toast.makeText(MainActivity.this, page, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
+
+
 }
