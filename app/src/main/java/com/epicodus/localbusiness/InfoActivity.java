f798eee124a7bf024ee6ea038ac871ea3f28e089
Abigail Rolling and Brad London <arolling@gmail.com>
2016-04-21 15:08:23 -0700

    create individual page views depending on drawer choice

--- app/src/main/java/com/epicodus/localbusiness/InfoActivity.java
+++ app/src/main/java/com/epicodus/localbusiness/InfoActivity.java
@@ -3,14 +3,20 @@ package com.epicodus.localbusiness;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
+import android.util.Log;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 import android.widget.TextView;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class InfoActivity extends AppCompatActivity {
-    @Bind(R.id.tvTitle) TextView mTvTitle;
+    public static final String TAG = InfoActivity.class.getSimpleName();
 
+    @Bind(R.id.tvPageTitle) TextView mPageTitle;
+    @Bind(R.id.tvDescription) TextView mDescription;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +26,35 @@ public class InfoActivity extends AppCompatActivity {
 
         Intent intent = getIntent();
         String page = intent.getStringExtra("page");
-        mTvTitle.setText(page);
+        Log.d(TAG, page);
+        mDescription.setMovementMethod(LinkMovementMethod.getInstance());
+        mPageTitle.setText(page);
+        int content;
+        switch(page){
+            case "Home":
+                content = R.string.home_page;
+                break;
+            case "What We Do":
+                content = R.string.what_we_do;
+                break;
+            case "Get Involved":
+                content = R.string.get_involved;
+                break;
+            case "How to Give":
+                content = R.string.how_to_give;
+                break;
+            case "Multi-Media":
+                content = R.string.multi_media;
+                break;
+            case "About Us":
+                content = R.string.about_us;
+                break;
+            case "Blog":
+                content = R.string.blog;
+                break;
+            default:
+                content = R.string.home_page;
+        }
+        mDescription.setText(content);
     }
 }
