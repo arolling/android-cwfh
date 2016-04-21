ae97e0468a023e0eb6e573b9f1ff0779aaa863fb
Abigail Rolling and Brad London <london.brad@gmail.com>
2016-04-21 10:19:17 -0700

    Make functioning drawer nav bar

--- app/src/main/java/com/epicodus/localbusiness/MainActivity.java
+++ app/src/main/java/com/epicodus/localbusiness/MainActivity.java
@@ -2,12 +2,29 @@ package com.epicodus.localbusiness;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String TAG = MainActivity.class.getSimpleName();
+    @Bind(R.id.navList) ListView mNavList;
+    private ArrayAdapter<String> mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        addDrawerItems();
+    }
+
+    private void addDrawerItems(){
+        String[] navArray = { "Home", "What We Do", "Get Involved", "How to Give", "Multi-Media", "About Us", "Blog", "Personal Blog"};
+        mAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, navArray);
+        mNavList.setAdapter(mAdapter);
     }
 }
